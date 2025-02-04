ff70188f2163d428832a8703f1e239cf109d7a6d
xeniape <xenia.fischer@stackable.tech>
2025-02-04 12:56:39 +0100

    chore: update to oci references

--- .scripts/get_manifest_digest.sh
+++ .scripts/get_manifest_digest.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Get a manifest digest. Example inputs:
-# - docker.stackable.tech/stackable/hello-world:0.0.1-SNAPSHOT-stackable0.0.0-dev
-# - docker.stackable.tech/stackable/hello-world:0.0.1-SNAPSHOT-stackable0.0.0-dev-amd64
+# - oci.stackable.tech/sdp/hello-world:0.0.1-SNAPSHOT-stackable0.0.0-dev
+# - oci.stackable.tech/sdp/hello-world:0.0.1-SNAPSHOT-stackable0.0.0-dev-amd64
 set -euo pipefail
 
 # Note: `docker manifest push` currently outputs the same hash, but `manifest`
