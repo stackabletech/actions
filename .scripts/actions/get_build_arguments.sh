ca017dbf220f77ca61abf12fc5317c462c668ea7
Techassi <git@techassi.dev>
2025-08-28 14:21:15 +0200

    feat(build-container-image): Support Docker build arguments (#70)

---
+++ .scripts/actions/get_build_arguments.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Splits comma-separated values, prepends them with --build-arg, and prints
+# them out separated by newlines.
+echo "$1" | awk '{ split($0, args, ","); for (i in args) { printf "--build-arg \"%s\" ", args[i] }}'
