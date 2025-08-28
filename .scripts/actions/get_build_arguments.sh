949b43e332652402e6c16b2c0ce4403473866933
Techassi <git@techassi.dev>
2025-08-28 09:53:32 +0200

    chore: Quote build args, don't print newlines

--- .scripts/actions/get_build_arguments.sh
+++ .scripts/actions/get_build_arguments.sh
@@ -2,4 +2,4 @@
 
 # Splits comma-separated values, prepends them with --build-arg, and prints
 # them out separated by newlines.
-echo "$1" | awk -F',' '{ split($0, args, ","); for (i in args) { printf "--build-arg %s\n", args[i] }}'
+echo "$1" | awk '{ split($0, args, ","); for (i in args) { printf "--build-arg \"%s\" ", args[i] }}'
