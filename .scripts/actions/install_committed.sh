ac87e92f0fc95ddb3c78b91d776b7358c1e011cc
Techassi <git@techassi.dev>
2026-06-02 16:20:24 +0200

    fix: Properly using the latest version is basically impossible

--- .scripts/actions/install_committed.sh
+++ .scripts/actions/install_committed.sh
@@ -6,11 +6,7 @@ set -euo pipefail
 ARCH=$(uname -m)
 mkdir /tmp/committed
 
-if [ "$COMMITTED_VERSION" == "latest" ]; then
-  curl -fsSL -o /tmp/committed/committed.tar.gz "https://github.com/crate-ci/committed/releases/latest/download/committed-${ARCH}-unknown-linux-musl.tar.gz"
-else
-  curl -fsSL -o /tmp/committed/committed.tar.gz "https://github.com/crate-ci/committed/releases/download/committed-${COMMITTED_VERSION}/committed-${ARCH}-unknown-linux-musl.tar.gz"
-fi
+curl -fsSL -o /tmp/committed/committed.tar.gz "https://github.com/crate-ci/committed/releases/download/${COMMITTED_VERSION}/committed-${COMMITTED_VERSION}-${ARCH}-unknown-linux-musl.tar.gz"
 
 tar --directory="/tmp/committed" -zxvf /tmp/committed/committed.tar.gz ./committed
 sudo install -m 755 -t /usr/local/bin /tmp/committed/committed
