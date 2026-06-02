987db4ae9e59efb5b4483eec92508ad8248b8ec3
Techassi <git@techassi.dev>
2026-06-02 16:08:15 +0200

    ci: Properly untar release

--- .scripts/actions/install_committed.sh
+++ .scripts/actions/install_committed.sh
@@ -4,12 +4,14 @@ set -euo pipefail
 [ -n "${RUNNER_DEBUG+set}" ] && set -x
 
 ARCH=$(uname -m)
+mkdir /tmp/committed
 
 if [ "$COMMITTED_VERSION" == "latest" ]; then
-  curl -fsSL -o /tmp/committed "https://github.com/crate-ci/committed/releases/latest/download/committed-${ARCH}-unknown-linux-gnu"
+  curl -fsSL -o /tmp/committed/committed.tar.gz "https://github.com/crate-ci/committed/releases/latest/download/committed-${ARCH}-unknown-linux-gnu.tar.gz"
 else
-  curl -fsSL -o /tmp/committed "https://github.com/crate-ci/committed/releases/download/committed-${COMMITTED_VERSION}/committed-${ARCH}-unknown-linux-gnu"
+  curl -fsSL -o /tmp/committed/committed.tar.gz "https://github.com/crate-ci/committed/releases/download/committed-${COMMITTED_VERSION}/committed-${ARCH}-unknown-linux-gnu.tar.gz"
 fi
 
-sudo install -m 755 -t /usr/local/bin /tmp/committed
+tar --directory="/tmp/committed" -zxvf /tmp/committed/committed.tar.gz ./committed
+sudo install -m 755 -t /usr/local/bin /tmp/committed/committed
 rm -rf /tmp/committed
