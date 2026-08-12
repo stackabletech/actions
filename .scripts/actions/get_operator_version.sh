865049625507c74372d19bc7890ab13e5b428b69
Techassi <git@techassi.dev>
2026-08-12 16:47:30 +0200

    feat(run-integration-test): Check if selected operator PR version exists
    The get_operator_version script now checks of the selected operator
    PR version exists before returning that version to be used in the
    integration test. If that version does not exist (yet), it prints
    a warning and falls back to 0.0.0-dev instead.

--- .scripts/actions/get_operator_version.sh
+++ .scripts/actions/get_operator_version.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
 set -uo pipefail
 
-if [ "$1"  == "main" ]; then
+# This script takes two arguments:
+# 1. The operator name, like zookeeper-operator
+# 2. The ref name, like `main` or a tag
+
+if [ "$2"  == "main" ]; then
   echo "0.0.0-dev"
   exit
 fi
 
-PR_NUMBER=$(gh pr view "$1" --json number --jq '.number')
+PR_NUMBER=$(gh pr view "$2" --json number --jq '.number')
 
-if [ "$?" != "0" ]; then
-  echo "0.0.0-dev"
+if [ "$?" == "0" ]; then
+  VERSION="0.0.0-pr$PR_NUMBER"
+  # Check if the image exists upstream. If not, fall back to 0.0.0-dev
+  HTTP_STATUS_CODE=$(curl -o /dev/null -w "%{http_code}" -H "Authorization: Does not matter, but Harbor needs it" "https://oci.stackable.tech/v2/sdp/$1/manifests/${VERSION}")
+
+  if [ "$HTTP_STATUS_CODE" == "200" ]; then
+    echo "$VERSION"
+  else
+    # The PR version is no (yet) available. Print a warning and fall back to 0.0.0-dev
+    echo "::warning title=Operator Version::The operator PR version was not available (yet). Fell back to 0.0.0-dev" 1>&2
+    echo "0.0.0-dev"
+  fi
 else
-  echo "0.0.0-pr$PR_NUMBER"
+  echo "0.0.0-dev"
 fi
