271ee73de807b4fe70caa3efb1a26870b6bdc9b1
Techassi <git@techassi.dev>
2026-08-13 09:43:25 +0200

    refactor(run-integration-test): Explicitly set repo for gh command

--- .scripts/actions/get_operator_version.sh
+++ .scripts/actions/get_operator_version.sh
@@ -2,10 +2,12 @@
 set -euo pipefail
 
 # This script takes two arguments:
-# 1. The operator name, like zookeeper-operator
-# 2. The ref name, like `main` or a tag
-OPERATOR_NAME="$1"
-REF_NAME="$2"
+# 1. The complete repository name, like `stackabletech/zookeeper-operator`
+# 2. The operator name, like zookeeper-operator
+# 3. The ref name, like `main` or a tag
+REPOSITORY="$1"
+OPERATOR_NAME="$2"
+REF_NAME="$3"
 
 DEFAULT_VERSION="0.0.0-dev"
 
@@ -16,7 +18,7 @@ fi
 
 # Look up the PR number for the ref. If no PR exists, this exists with a non-0
 # exit code which we handle below.
-if PR_NUMBER=$(gh pr view "${REF_NAME}" --json number --jq '.number'); then
+if PR_NUMBER=$(gh pr view "${REF_NAME}" --repo "${REPOSITORY}" --json number --jq '.number'); then
   VERSION="0.0.0-pr$PR_NUMBER"
   # Check if the image exists upstream. If not, fall back to 0.0.0-dev
   HTTP_STATUS_CODE=$(curl -o /dev/null -w "%{http_code}" -H "Authorization: Does not matter, but Harbor needs it" "https://oci.stackable.tech/v2/sdp/${OPERATOR_NAME}/manifests/${VERSION}")
